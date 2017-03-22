749f6d7ee644b4bbd3b049a142636aabc2ce5190
Blake Womack and Grace Stuart <blake+grace@>
2017-03-22 13:07:46 -0700

    add code for second test.

--- src/test/java/JobOpeningTest.java
+++ src/test/java/JobOpeningTest.java
@@ -9,4 +9,10 @@ public class JobOpeningTest {
     assertEquals(true, newJob instanceof JobOpening);
   }
 
+  @Test
+  public void getJobTitle_addJobTitle_string() {
+    JobOpening newJob = new JobOpening("title", "description", "contact", "city");
+    assertEquals("title", newJob.getJobTitle());
+  }
+
 }
